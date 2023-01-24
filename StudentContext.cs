3988d35f699dfadf9217375904c9d53ed812af86
David Jarvis <cheesyd64@hotmail.com>
2023-01-24 13:01:14 -0800

    Added Entity Framework Sql Server package

--- StudentContext.cs
+++ StudentContext.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace Entity_Framework_core_example
 {
-    public class StudentContext
-    {
-        public StudentContext()
-        {
-        }
+    //EF Core Getting started
+    // https://docs.microsoft.com/en-us/ef/core/get-started/overview/install
+    internal class StudentContext : DbContext {
+
     }
 }
 
