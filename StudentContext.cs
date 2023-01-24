3cf7e91542387891da3d791a2c1b62937c290ada
David Jarvis <cheesyd64@hotmail.com>
2023-01-24 15:11:58 -0800

    Added EF Core Tools package and created student class

--- StudentContext.cs
+++ StudentContext.cs
@@ -5,8 +5,21 @@ namespace Entity_Framework_core_example
 {
     //EF Core Getting started
     // https://docs.microsoft.com/en-us/ef/core/get-started/overview/install
-    internal class StudentContext : DbContext {
+    public class StudentContext : DbContext {
 
+        public StudentContext()
+        {
+
+        }
+
+        //Database = desired name of database
+        //server = server we are connectinig to localdb is included with visual studio
+        //Trusted_Connection - indicates that our windows account should be used
+        protected override void OnConfigure(DbContextOptionsBuilder options) {
+            DbContextOptionsBuilder.UseSqlServer("Server = (localdb\\mssqllocaldb;" +
+                                                    "Database=EFCoreExample;" +
+                                                    "Trusted_Connection=True;");
+        }
     }
 }
 
