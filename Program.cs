a95375f2a098eb07550ceb79dc5e6fe11fd4e4e8
cheesy d <cheesyd64@hotmail.com>
2023-01-24 16:39:49 -0800

    Added test code for create and select

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Entity_Framework_core_example;
+
+using StudentContext dbContext = new();
+
+//add with ef core
+Student s = new() {
+	FullName = "David Poop",
+	EmailAddress = "Email@email.com",
+	DateOfBirth = new DateTime(2000, 1, 12)
+};
+
+dbContext.Students.Add(s);//prepares students insert statement
+dbContext.SaveChanges();//executes prepared insert
+
+
+//retrieve students from database
+List<Student> allStudents = dbContext.Students.ToList();
+
+foreach(Student stu in allStudents) {
+	Console.WriteLine($"{stu.FullName} has an email of {stu.EmailAddress}");
+	Console.WriteLine();
+}
 
